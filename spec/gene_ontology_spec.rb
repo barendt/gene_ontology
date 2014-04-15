ce324560c20f98760d06133e1fe9b82745a8aec9
Greg Barendt <gregory.barendt@icloud.com>
2014-04-14 23:28:11 -0400

    Add Gemfile and use RSpec

--- spec/gene_ontology_spec.rb
+++ spec/gene_ontology_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe "GeneOntology" do
   it "fails" do
-    should.flunk "hey buddy, you should probably rename this file and start specing for real"
+    pending "hey buddy, you should probably rename this file and start specing for real"
   end
 end
